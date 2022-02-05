af0026dc3d09da78b7c30bea15b8fe7fee0e2082
perstarkse <perstark.se@gmail.com>
2022-02-05 15:13:24 +0100

    spring cleaning
    clearing out everything but the source files

---
+++ week two/whileLoop/whileLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class WhileLoop
+{
+    static void Main()
+    {
+        bool köra = true;
+
+        while (köra == true)
+        {
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1: 
+                    Console.WriteLine("du tryckte på 1"); 
+                    break; 
+                case 2:
+                    Console.WriteLine("du tryckte på 2");
+                    break;
+                case 3:
+                    köra = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ week two/ålderprogram/ålderprogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class ålderprogram
+{
+    static void Main()
+    {
+        // frågar efter och tar emot ålder i heltal, sparar i variabel
+        Console.WriteLine("ange ålder i heltal");
+        int ålder = int.Parse(Console.ReadLine());
+
+        //testar ålder mot givna parametrar
+        if (ålder == 18)
+        {
+            Console.WriteLine("Du har angett att du är 18 år gammal, dags att bli vuxen?");
+        }
+        else if (ålder > 18)
+        {
+            Console.WriteLine("Du har angett att du är :" + ålder + " år gammal");
+        }
+    }  
+
+}
