0118b450c807d341dd88797cddd9023bf5f9982f
perstarkse <perstark.se@gmail.com>
2022-02-05 14:42:32 +0100

    week 1
    finished week one tasks, getting an intro to c# syntax

---
+++ glazerCalc/GlazerCalc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class GlazerCalc
+{
+	static void Main()
+	{
+		double width, height, woodLength, glassArea;
+		string widthString, heightString;
+
+		Console.WriteLine("input width: ");
+        widthString = Console.ReadLine();
+		width = double.Parse(widthString);
+		Console.WriteLine("input height:");
+		heightString = Console.ReadLine();
+		height = double.Parse(heightString);
+
+		woodLength = 2 * (width + height) * 3.25;
+		glassArea = 2 * (height * width);
+
+		Console.WriteLine("The length of the wood is " + woodLength + " meters");
+		Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+	}
+}
---
+++ variabler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Konvertering
+{
+    static void Main()
+    {
+        Console.WriteLine("ange heltal ett:");
+        int heltalEtt = int.Parse(Console.ReadLine());
+        Console.WriteLine("ange heltal två:");
+        int heltalTvå = int.Parse(Console.ReadLine());
+        Console.WriteLine(heltalEtt * heltalTvå);
+    }
+}
