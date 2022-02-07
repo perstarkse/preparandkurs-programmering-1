5bd355f008be283ee6b6157c7bea630e42d6fb4e
perstarkse <perstark.se@gmail.com>
2022-02-07 20:23:16 +0100

    vektorer och loops
    arbetar vidare med övningarna

---
+++ vecka fyra/forvektorer/forvektorer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ForVektorer
+{
+    static void Main()
+    {
+        int.TryParse(Console.ReadLine(), out int antalVektorer);
+        double[] vektorLista = new double[antalVektorer];
+        for (int i = 0; i < vektorLista.Length; i++)
+        {
+            double.TryParse(Console.ReadLine(),out double _input);
+            vektorLista[i] = _input;
+        }
+        double totalsumma = 0;
+        for (int i = 0;i < vektorLista.Length; i++)
+        {
+            totalsumma += vektorLista[i];
+        }
+        Console.WriteLine(totalsumma);
+    }
+}
---
+++ vecka fyra/listor/listor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Listor
+{
+    static void Main()
+    {
+        List<string> strängLista = new List<string>();
+        Console.WriteLine("välj hur många strängar du vill skriva in");
+        int.TryParse(Console.ReadLine(), out int val);
+        Console.WriteLine("du har valt att skriva in " + (val) + " entries");
+        for (int i = 0; i < val; i++)
+        {
+            strängLista.Add(Console.ReadLine());
+        }
+        for (int i = 0; i < val; i++)
+        {
+            Console.WriteLine(strängLista[i]);
+        }
+    }
+}
