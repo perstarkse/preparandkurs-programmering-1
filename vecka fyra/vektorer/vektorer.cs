f2c28a6ea33213fa64682e02515bd0e9cb55f520
perstarkse <perstark.se@gmail.com>
2022-02-07 09:16:56 +0100

    vecka fyra fortsätter
    vektorer, dvs arrays ;p

---
+++ vecka fyra/vektorer/vektorer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Vektorer
+{
+    static int Addera(int[] siffor)
+    {
+        int totalsumma = 0;
+        for (int i = 0;i < siffor.Length;i++)
+        {
+            totalsumma = totalsumma + siffor[i];
+        }
+        return totalsumma;
+    }
+
+    static void Main()
+    {
+        int[] sifferarray = new int[5];
+        for (int i = 0; i < 5; i++) {
+            int.TryParse(Console.ReadLine(), out sifferarray[i]);
+        }
+        Console.WriteLine(Addera(sifferarray));
+    }
+}
